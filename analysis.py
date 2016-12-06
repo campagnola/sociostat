013b235db438ef67112f8aad14c62745910b9d57
Luke Campagnola <luke.campagnola@gmail.com>
2016-12-06 09:23:40 -0800

    update analysis script

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.stats
 import pyqtgraph as pg
 import csv
 
@@ -60,6 +61,35 @@ for i,line in enumerate(lines):
     map_data[i] = tuple([np.nan if (dtype[j][1] is float and x in ('NA' or '')) else x for j,x in enumerate(line)])
 
 
+b_w_pop = map_data['BAC_TOT'] / map_data['WA_TOT']
+
+
+black_shot = map_data['BlackArmed'] + map_data['BlackUnarmed']
+white_shot = map_data['WhiteArmed'] + map_data['WhiteUnarmed']
+
+b_w_shot = black_shot / white_shot
+
+#pg.plot(white_shot, black_shot, pen=None, symbol='o')
+
+
+
+b_w_assault = (map_data['AssaultsBlack.sum'] / map_data['BAC_TOT']) / (map_data['AssaultsWhite.sum'] / map_data['WA_TOT'])
+
+
+mask = np.isfinite(b_w_shot) & np.isfinite(b_w_assault)
+
+x = b_w_assault[mask]
+y = b_w_shot[mask] / b_w_pop[mask]
+
+lx = np.log(x)
+ly = np.log(y)
+mask2 = np.isfinite(lx) & np.isfinite(ly)
+r,p = scipy.stats.pearsonr(lx[mask2], ly[mask2])
+
+plt = pg.plot(x, y, pen=None, symbol='o', symbolSize=(black_shot + white_shot)[mask] + 5, 
+              title="black:white assault vs killing (r=%0.2f p=%0.2f)" % (r, p),
+              labels={'left': 'black:white shot normalized by population', 
+                      'bottom': 'black:white assaults normalized by population'})
 
 
 
